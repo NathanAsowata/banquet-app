fbebb6d6c003bf59d46d2fdcc68f51ce40ed5bc1
Nathan Asowata <nathanasowata@gmail.com>
2022-07-11 19:01:18 +0100

    Completed search bar feature

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,12 +1,33 @@
+import { ChangeEvent, useState } from "react";
 import styles from "../styles/SearchBar.module.scss"
+import Link from "next/link";
 
 const SearchBar = () => {
+
+  const [userInput, setUserInput] = useState("");
+
+  const handleUserInput = (event: ChangeEvent<HTMLInputElement>) => {
+    setUserInput(event.target.value)
+  }
+
+  const handleSearch = (event: { preventDefault: () => void; }) => {
+      if (userInput === "") {
+        event.preventDefault();
+        alert("Type your search")
+      }
+  }
+
   return (
     <form className={styles.form}>
-        <input className={styles.input} placeholder="Find a recipe..." />
-        <button className={styles.button}>
-          <span className={styles.text}>Search</span>
-        </button>
+        <input className={styles.input} 
+          placeholder="Find a recipe..." 
+          value={userInput} 
+          onChange={(e) => handleUserInput(e)}
+          required
+        />
+        <Link href={`/search/${userInput}`}>
+          <button type="submit" className={styles.button} onClick={handleSearch}> Search</button>
+        </Link>
     </form>
   )
 }
--- pages/search/[userInput].tsx
+++ pages/search/[userInput].tsx
@@ -1,6 +1,6 @@
-import styles from "../styles/search.module.scss"
-import SearchBar from '../components/SearchBar'
-import SearchResult from "../components/SearchResult"
+import styles from "../../styles/search.module.scss"
+import SearchBar from '../../components/SearchBar'
+import SearchResult from "../../components/SearchResult"
 import Head from "next/head"
 
 const search = () => {

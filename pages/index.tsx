946686500e27c954d662c4d45ecf600244ee9a63
Nathan Asowata <nathanasowata@gmail.com>
2022-08-08 15:04:17 +0100

    Updated home page design

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,14 +27,14 @@ const Home= ({recipes}:propTypes) => {
         or just trying to figure out what to cook for dinner, our app has got you covered</p>
       <SearchBar />
     </header>
-      <h2 className={styles.category}>Popular recipes</h2>
-      <section className={styles.content}>
-      {recipes.meals.map(meal => {
-        return (
-          <Recipe key={meal.idMeal} mealDetails={meal} />
-        )
-      })}
-      </section>
+        <h2 className={styles.category}>Popular recipes</h2>
+        <section className={styles.content}>
+          {recipes.meals.map(meal => {
+            return (
+              <Recipe key={meal.idMeal} mealDetails={meal} />
+            )
+          })}
+        </section>
     </>
   )
 }
